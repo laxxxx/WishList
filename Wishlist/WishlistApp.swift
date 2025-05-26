26001d2a134f1bfb37079d579ea3b454fdcc4323
laxxxx <lakshmancool9@gmail.com>
2025-05-26 20:50:42 +0530

    initial commit

---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Sree Lakshman on 26/05/25.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
